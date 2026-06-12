ebb31998054f7f1126ff19a8a22f9db5eabed2fc
celine <selinnisabaykar3@gmail.com>
2026-06-12 12:14:41 +0300

    test api

--- api/ask.js
+++ api/ask.js
@@ -1,38 +1,6 @@
 export default async function handler(req, res) {
-
-try {
-
-const response = await fetch(
-  "https://api.groq.com/openai/v1/chat/completions",
-  {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      model: "llama-3.3-70b-versatile",
-      messages: [
-        {
-          role: "user",
-          content: "Merhaba"
-        }
-      ]
-    })
-  }
-);
-
-const text = await response.text();
-
-return res.status(200).send(text);
-
-
-} catch (error) {
-
-return res.status(500).json({
-  error: error.message,
-  stack: error.stack
+return res.status(200).json({
+success: true,
+message: "API çalışıyor"
 });
-}
-
 }
