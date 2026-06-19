88217415944bcfea088492785824842cd465ec39
celine <selinnisabaykar3@gmail.com>
2026-06-19 13:47:03 +0300

    test api

--- api/upload.js
+++ api/upload.js
@@ -1,35 +1,10 @@
-import { put } from "@vercel/blob";
-
-export const config = {
-  api: {
-    bodyParser: false
-  }
-};
-
 export default async function handler(req, res) {
 
   try {
 
-    if (req.method !== "POST") {
-      return res.status(405).json({
-        error: "Method not allowed"
-      });
-    }
-
-    const filename =
-      req.headers["x-filename"];
-
-    const blob = await put(
-      filename,
-      req,
-      {
-        access: "public"
-      }
-    );
-
     return res.status(200).json({
       success: true,
-      url: blob.url
+      step: "api çalışıyor"
     });
 
   } catch (error) {
