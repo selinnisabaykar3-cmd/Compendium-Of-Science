dac5b496f35dbbd4aeac271c5f0806b4318ebaaa
celine <selinnisabaykar3@gmail.com>
2026-06-20 20:36:54 +0300

    blob token test

--- api/upload.js
+++ api/upload.js
@@ -1,25 +1,8 @@
-import { put } from "@vercel/blob";
-
-export const config = {
-  api: {
-    bodyParser: false
-  }
-};
-
 export default async function handler(req, res) {
 
-  try {
-
-    return res.status(200).json({
-      hasToken: !!process.env.BLOB_READ_WRITE_TOKEN
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-
-  }
+  return res.status(200).json({
+    keys: Object.keys(process.env)
+      .filter(key => key.includes("BLOB"))
+  });
 
 }
