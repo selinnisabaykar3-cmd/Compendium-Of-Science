87ca74a0d35bbeeb5ef38d6d5d65a7f21a04a023
celine <selinnisabaykar3@gmail.com>
2026-06-21 20:11:49 +0300

    add random suffix

--- api/upload.js
+++ api/upload.js
@@ -25,10 +25,10 @@ export default async function handler(req, res) {
   filename,
   req,
   {
-    access: "private"
+    access: "private",
+    addRandomSuffix: true
   }
 );
-
     return res.status(200).json({
       success: true,
       url: blob.url
